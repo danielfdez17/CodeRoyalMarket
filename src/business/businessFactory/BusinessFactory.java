ba3610a32675503a39260351c3258c06144300b2
danielfdez17 <daniel17dev@gmail.com>
2023-12-23 19:42:37 +0100

    businessFactory done

--- src/business/businessFactory/BusinessFactory.java
+++ src/business/businessFactory/BusinessFactory.java
@@ -7,55 +7,19 @@ import business.sale.SaleAS;
 import business.warehouse.WarehouseAS;
 import business.worker.WorkerAS;
 
-public class BusinessFactory {
+public abstract class BusinessFactory {
+	
 	private static BusinessFactory instance;
 
 	public static BusinessFactory getInstance() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		if (instance == null) instance = new BusinessFactoryImp();
+		return instance;
 	}
 
-	public ClientAS createClientAS() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	public ProductAS createProductAS() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	public ProviderAS createProviderAS() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	public SaleAS createSaleAS() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	public WarehouseAS createWarehouseAS() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	public WorkerAS createWorkerAS() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
+	public abstract ClientAS createClientAS();
+	public abstract ProductAS createProductAS();
+	public abstract ProviderAS createProviderAS();
+	public abstract SaleAS createSaleAS();
+	public abstract WarehouseAS createWarehouseAS();
+	public abstract WorkerAS createWorkerAS();
 }
--- src/business/businessFactory/BusinessFactoryImp.java
+++ src/business/businessFactory/BusinessFactoryImp.java
@@ -1,4 +1,47 @@
 package business.businessFactory;
 
+import business.client.ClientAS;
+import business.client.ClientASImp;
+import business.product.ProductAS;
+import business.product.ProductASImp;
+import business.provider.ProviderAS;
+import business.provider.ProviderASImp;
+import business.sale.SaleAS;
+import business.sale.SaleASImp;
+import business.warehouse.WarehouseAS;
+import business.warehouse.WarehouseASImp;
+import business.worker.WorkerAS;
+import business.worker.WorkerASImp;
+
 public class BusinessFactoryImp extends BusinessFactory {
+
+	@Override
+	public ClientAS createClientAS() {
+		return new ClientASImp();
+	}
+
+	@Override
+	public ProductAS createProductAS() {
+		return new ProductASImp();
+	}
+
+	@Override
+	public ProviderAS createProviderAS() {
+		return new ProviderASImp();
+	}
+
+	@Override
+	public SaleAS createSaleAS() {
+		return new SaleASImp();
+	}
+
+	@Override
+	public WarehouseAS createWarehouseAS() {
+		return new WarehouseASImp();
+	}
+
+	@Override
+	public WorkerAS createWorkerAS() {
+		return new WorkerASImp();
+	}
 }
