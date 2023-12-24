24c31c3ca2396219c5a25b414447f0c5313e7696
danielfdez17 <daniel17dev@gmail.com>
2023-12-24 09:59:59 +0100

    product transfer & BO

--- src/business/product/ProductBO.java
+++ src/business/product/ProductBO.java
@@ -1,11 +1,16 @@
 package business.product;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 import java.io.Serializable;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
+import javax.persistence.Version;
 import javax.persistence.NamedQueries;
-import java.util.Set;
+import java.util.List;
 import business.provider.ProviderBO;
 import javax.persistence.ManyToMany;
 import business.warehouse.WarehouseBO;
@@ -20,35 +25,100 @@ import javax.persistence.ManyToOne;
 		@NamedQuery(name = "business.product.ProductBO.findByprice", query = "select obj from ProductBO obj where :price = obj.price "),
 		@NamedQuery(name = "business.product.ProductBO.findByactive", query = "select obj from ProductBO obj where :active = obj.active "),
 		@NamedQuery(name = "business.product.ProductBO.findByproviderBO", query = "select obj from ProductBO obj where :providerBO MEMBER OF obj.providerBO "),
-		@NamedQuery(name = "business.product.ProductBO.findBywarehouseBO", query = "select obj from ProductBO obj where :warehouseBO = obj.warehouseBO ") })
+		@NamedQuery(name = "business.product.ProductBO.findBywarehouseBO", query = "select obj from ProductBO obj where :warehouseBO = obj.warehouseBO "),
+		@NamedQuery(name = "business.product.ProductBO.findAll", query = "select obj from ProductBO obj"),
+})
 public class ProductBO implements Serializable {
 	private static final long serialVersionUID = 0;
 
-	public ProductBO() {
-	}
+	public ProductBO() {}
 
-	@Id
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Version
 	private int version;
+	@Column(unique = true, nullable = false)
 	private String name;
 	private int stock;
 	private double price;
 	private boolean active;
-	@ManyToMany(mappedBy = "productBO")
-	private Set<ProviderBO> providerBO;
+	
+	@ManyToMany()
+	private List<ProviderBO> providerBO;
 	@ManyToOne
 	private WarehouseBO warehouseBO;
+	
+	public ProductBO(String name, int stock, double price) {
+		super();
+	}
+	
+	public ProductBO(int id, String name, int stock, double price, boolean active) {
+		this(name, stock, price);
+		this.setId(id);
+		this.setActive(active);
+	}
 
 	public ProductBO(ProductTransfer product) {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
+		this(product.getId(), product.getName(), product.getStock(), product.getPrice(), product.isActive());
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public List<ProviderBO> getProviderBO() {
+		return providerBO;
+	}
+
+	public void setProviderBO(List<ProviderBO> providerBO) {
+		this.providerBO = providerBO;
+	}
+
+	public WarehouseBO getWarehouseBO() {
+		return warehouseBO;
+	}
+
+	public void setWarehouseBO(WarehouseBO warehouseBO) {
+		this.warehouseBO = warehouseBO;
 	}
 
 	public ProductTransfer toTransfer() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return new ProductTransfer(id, name, stock, price, warehouseBO.getId(), active);
 	}
 }
--- src/business/product/ProductTransfer.java
+++ src/business/product/ProductTransfer.java
@@ -1,5 +1,7 @@
 package business.product;
 
+import utilities.Utils;
+
 public class ProductTransfer {
 	private int id;
 	private String name;
@@ -7,4 +9,81 @@ public class ProductTransfer {
 	private double price;
 	private int warehouseId;
 	private boolean active;
+	
+	public ProductTransfer(String name, int stock, double price, int warehouseId) {
+		super();
+		this.name = name;
+		this.stock = stock;
+		this.price = price;
+		this.warehouseId = warehouseId;
+	}
+
+	public ProductTransfer(int id, String name, int stock, double price, int warehouseId, boolean active) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.stock = stock;
+		this.price = price;
+		this.warehouseId = warehouseId;
+		this.active = active;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getWarehouseId() {
+		return warehouseId;
+	}
+
+	public void setWarehouseId(int warehouseId) {
+		this.warehouseId = warehouseId;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + this.id + Utils.JUMP +
+				"Name: " + this.name + Utils.JUMP +
+				"Stock: " + this.stock + Utils.JUMP +
+				"Price: " + this.price + Utils.JUMP +
+				"Warehouse ID: " + this.warehouseId + Utils.JUMP +
+				"Active: " + (this.active ? "yes" : "no") + Utils.JUMP;
+	}
+	
 }
