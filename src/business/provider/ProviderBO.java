947a79b84e0c9852ec11d8fbe165d64c44a493ee
danielfdez17 <daniel17dev@gmail.com>
2023-12-24 10:00:24 +0100

    provider transfer & BO

--- src/business/provider/ProviderBO.java
+++ src/business/provider/ProviderBO.java
@@ -1,11 +1,17 @@
 package business.provider;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 import java.io.Serializable;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
+import javax.persistence.Version;
 import javax.persistence.NamedQueries;
-import java.util.Set;
+
+import java.util.List;
 import business.product.ProductBO;
 import javax.persistence.ManyToMany;
 
@@ -16,32 +22,83 @@ import javax.persistence.ManyToMany;
 		@NamedQuery(name = "business.provider.ProviderBO.findByname", query = "select obj from ProviderBO obj where :name = obj.name "),
 		@NamedQuery(name = "business.provider.ProviderBO.findByphoneNumber", query = "select obj from ProviderBO obj where :phoneNumber = obj.phoneNumber "),
 		@NamedQuery(name = "business.provider.ProviderBO.findByactive", query = "select obj from ProviderBO obj where :active = obj.active "),
-		@NamedQuery(name = "business.provider.ProviderBO.findByproductBO", query = "select obj from ProviderBO obj where :productBO MEMBER OF obj.productBO ") })
+		@NamedQuery(name = "business.provider.ProviderBO.findByproductBO", query = "select obj from ProviderBO obj where :productBO MEMBER OF obj.productBO "),
+		@NamedQuery(name = "business.provider.ProviderBO.findAll", query = "select obj from ProviderBO"),
+})
 public class ProviderBO implements Serializable {
 	private static final long serialVersionUID = 0;
 
-	public ProviderBO() {
-	}
+	public ProviderBO() {}
 
-	@Id
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Version
 	private int version;
+	@Column(unique = true, nullable = false)
 	private String name;
 	private int phoneNumber;
 	private boolean active;
-	@ManyToMany
-	private Set<ProductBO> productBO;
+	@ManyToMany(mappedBy = "providerBO")
+	private List<ProductBO> productBO;
+	
+	public ProviderBO(String name, int phoneNumber) {
+		super();
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.active = true;
+	}
+	
+	public ProviderBO(int id, String name, int phoneNumber, boolean active) {
+		this(name, phoneNumber);
+		this.id = id;
+		this.active = active;
+	}
 
 	public ProviderBO(ProviderTransfer provider) {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
+		this(provider.getId(), provider.getName(), provider.getPhoneNumber(), provider.isActive());
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public List<ProductBO> getProductBO() {
+		return productBO;
+	}
+
+	public void setProductBO(List<ProductBO> productBO) {
+		this.productBO = productBO;
 	}
 
 	public ProviderTransfer toTransfer() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return new ProviderTransfer(id, name, phoneNumber, active);
 	}
 }
--- src/business/provider/ProviderTransfer.java
+++ src/business/provider/ProviderTransfer.java
@@ -1,8 +1,65 @@
 package business.provider;
 
+import utilities.Utils;
+
 public class ProviderTransfer {
 	private int id;
 	private String name;
 	private int phoneNumber;
 	private boolean active;
+	
+	public ProviderTransfer(String name, int phoneNumber) {
+		super();
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public ProviderTransfer(int id, String name, int phoneNumber, boolean active) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.active = active;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + this.id + Utils.JUMP +
+				"Name: " + this.name + Utils.JUMP +
+				"Phone number: " + this.phoneNumber + Utils.JUMP +
+				"Active: " + (this.active ? "yes" : "no") + Utils.JUMP;
+	}
+	
 }
