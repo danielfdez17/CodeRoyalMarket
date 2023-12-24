e9c1ecbb68b9b5e4b2c5bce8735827c02277d12f
danielfdez17 <daniel17dev@gmail.com>
2023-12-24 10:00:46 +0100

    sale transfer & BO

--- src/business/sale/SaleBO.java
+++ src/business/sale/SaleBO.java
@@ -1,12 +1,18 @@
 package business.sale;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 import java.io.Serializable;
+import java.sql.Date;
+
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.NamedQueries;
 import business.client.ClientBO;
 import javax.persistence.OneToOne;
+import javax.persistence.Version;
 
 @Entity
 @NamedQueries({
@@ -15,32 +21,79 @@ import javax.persistence.OneToOne;
 		@NamedQuery(name = "business.sale.SaleBO.findBycost", query = "select obj from SaleBO obj where :cost = obj.cost "),
 		@NamedQuery(name = "business.sale.SaleBO.findBydate", query = "select obj from SaleBO obj where :date = obj.date "),
 		@NamedQuery(name = "business.sale.SaleBO.findByactive", query = "select obj from SaleBO obj where :active = obj.active "),
-		@NamedQuery(name = "business.sale.SaleBO.findByclientBO", query = "select obj from SaleBO obj where :clientBO = obj.clientBO ") })
+		@NamedQuery(name = "business.sale.SaleBO.findByclientBO", query = "select obj from SaleBO obj where :clientBO = obj.clientBO "),
+		@NamedQuery(name = "business.sale.SaleBO.findAll", query = "select obj from SaleBO"),
+})
+
 public class SaleBO implements Serializable {
 	private static final long serialVersionUID = 0;
 
-	public SaleBO() {
-	}
+	public SaleBO() {}
 
-	@Id
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Version
 	private int version;
 	private double cost;
-	private Object date;
+	private Date date;
 	private boolean active;
 	@OneToOne
 	private ClientBO clientBO;
+	
+	
+	public SaleBO(int id, double cost, Date date, boolean active) {
+		super();
+		this.id = id;
+		this.cost = cost;
+		this.date = date;
+		this.active = active;
+	}
 
 	public SaleBO(SaleTransfer sale) {
-		// begin-user-code
-		// TODO Auto-generated constructor stub
-		// end-user-code
+		this(sale.getId(), sale.getCost(), sale.getDate(), sale.isActive());
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public ClientBO getClientBO() {
+		return clientBO;
+	}
+
+	public void setClientBO(ClientBO clientBO) {
+		this.clientBO = clientBO;
 	}
 
 	public SaleTransfer toTransfer() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		return new SaleTransfer(id, cost, date, clientBO.getId(), active);
 	}
 }
--- src/business/sale/SaleTransfer.java
+++ src/business/sale/SaleTransfer.java
@@ -2,10 +2,79 @@ package business.sale;
 
 import java.sql.Date;
 
+import utilities.Utils;
+
 public class SaleTransfer {
 	private int id;
 	private double cost;
 	private Date date;
 	private int clientId;
 	private boolean active;
+	
+	public SaleTransfer(int clientId) {
+		super();
+		this.clientId = clientId;
+		this.setCost(0.0);
+		this.setDate(new Date(System.currentTimeMillis()));
+		this.setActive(true);
+	}
+
+	public SaleTransfer(int id, double cost, Date date, int clientId, boolean active) {
+		super();
+		this.id = id;
+		this.cost = cost;
+		this.date = date;
+		this.clientId = clientId;
+		this.active = active;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + this.id + Utils.JUMP + 
+				"Cost: " + this.cost + Utils.JUMP +
+				"Date: " + this.date.toString() + Utils.JUMP +
+				"Client ID: " + this.clientId + Utils.JUMP +
+				"Active: " + (this.active ? "yes" : "no") + Utils.JUMP;
+	}
+	
 }
