db6c177ba850f2de5754ee745c42a0d3ed85f021
danielfdez17 <daniel17dev@gmail.com>
2023-12-24 18:32:18 +0100

    ASWarehouse seems to be done

--- src/business/warehouse/WarehouseAS.java
+++ src/business/warehouse/WarehouseAS.java
@@ -1,4 +1,13 @@
 package business.warehouse;
 
+import java.util.List;
+
 public interface WarehouseAS {
+	
+	public int createWarehouse(WarehouseTransfer warehouse);
+	public WarehouseTransfer readWarehouse(int warehouseId);
+	public List<WarehouseTransfer> readWarehouses();
+	public int updateWarehouse(WarehouseTransfer warehouse);
+	public int deleteWarehouse(int warehouseId);
+	
 }
--- src/business/warehouse/WarehouseASImp.java
+++ src/business/warehouse/WarehouseASImp.java
@@ -1,4 +1,187 @@
 package business.warehouse;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.LockModeType;
+import javax.persistence.TypedQuery;
+
+import business.entityManagerFactory.EMFFactory;
+import business.sintaxChecker.SintaxChecker;
+import utilities.BusinessException;
+import utilities.Errors;
+
 public class WarehouseASImp implements WarehouseAS {
+
+	@Override
+	public int createWarehouse(WarehouseTransfer warehouse) {
+		int res = Errors.SintaxError;
+		if (this.isValid(warehouse)) {
+			EntityManager em = EMFFactory.getInstance().createEntityManager();
+			EntityTransaction et = em.getTransaction();
+			et.begin();
+			try {
+				
+				BusinessException be = new BusinessException();
+				TypedQuery<WarehouseBO> query = em.createNamedQuery("business.warehouse.WarehouseBO.findByname", WarehouseBO.class);
+				query.setParameter("name", warehouse.getName());
+				WarehouseBO warehouseBO = null;
+				
+				if (query.getResultList().isEmpty()) {
+					warehouseBO = new WarehouseBO(warehouse);
+					em.persist(warehouseBO);
+					res = warehouseBO.getId();
+					warehouse.setId(res);
+				}
+				else {
+					warehouseBO = query.getResultList().get(0);
+					if (warehouseBO.isActive()) {
+						res = Errors.ActiveWarehouse;
+						throw be;
+					}
+					
+					warehouseBO = new WarehouseBO(warehouse.getName(), warehouse.getCity());
+					warehouseBO.setActive(true);
+					res = Errors.InactiveWarehouse;
+				}
+				
+				et.commit();
+				
+			} catch (Exception e) {
+				if (e instanceof BusinessException) {
+					et.rollback();
+				}
+				else {
+					res = Errors.UnespectedError;
+				}
+			} finally {
+				em.close();
+			}
+		}
+		return res;
+	}
+
+	@Override
+	public WarehouseTransfer readWarehouse(int warehouseId) {
+		WarehouseTransfer res = null;
+		EntityManager em = EMFFactory.getInstance().createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		et.begin();
+		try {
+			BusinessException be = new BusinessException();
+			WarehouseBO warehouseBO = em.find(WarehouseBO.class, warehouseId, LockModeType.OPTIMISTIC);
+			if (warehouseBO == null) {
+				throw be;
+			}
+			
+			et.commit();
+			res = warehouseBO.toTransfer();
+			
+		} catch(Exception e) {
+			if (e instanceof BusinessException) {
+				et.rollback();
+			}
+			res = null;
+		} finally {
+			em.close();
+		}
+		
+		return res;
+	}
+
+	@Override
+	public List<WarehouseTransfer> readWarehouses() {
+		List<WarehouseTransfer> res = new ArrayList<WarehouseTransfer>();
+		EntityManager em = EMFFactory.getInstance().createEntityManager();
+		TypedQuery<WarehouseBO> query = em.createNamedQuery("business.warehouse.WarehouseBO.findAll", WarehouseBO.class);
+		
+		for (WarehouseBO w : query.getResultList()) {
+			res.add(w.toTransfer());
+		}
+		
+		em.close();
+		return res;
+	}
+
+	@Override
+	public int updateWarehouse(WarehouseTransfer warehouse) {
+		int res = Errors.SintaxError;
+		if (this.isValid(warehouse)) {
+			EntityManager em = EMFFactory.getInstance().createEntityManager();
+			EntityTransaction et = em.getTransaction();
+			et.begin();
+			try {
+				
+				BusinessException be = new BusinessException();
+				WarehouseBO warehouseBO = em.find(WarehouseBO.class, warehouse.getId());
+				
+				if (warehouseBO == null) {
+					res = Errors.NonexistentWarehouse;
+					throw be;
+				}
+				
+				warehouseBO = new WarehouseBO(warehouse.getName(), warehouse.getCity());
+				et.commit();
+				res = warehouseBO.getId();
+				
+			} catch (Exception e) {
+				if (e instanceof BusinessException) {
+					et.rollback();
+				}
+				else {
+					res = Errors.UnespectedError;
+				}
+			} finally {
+				em.close();
+			}
+		}
+		return res;
+	}
+
+	@Override
+	public int deleteWarehouse(int warehouseId) {
+		int res = Errors.SintaxError;
+		EntityManager em = EMFFactory.getInstance().createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		et.begin();
+		try {
+			
+			BusinessException be = new BusinessException();
+			WarehouseBO warehouseBO = em.find(WarehouseBO.class, warehouseId);
+			
+			if (warehouseBO == null) {
+				res = Errors.NonexistentWarehouse;
+				throw be;
+			}
+			
+			if (!warehouseBO.isActive()) {
+				res = Errors.InactiveWarehouse;
+				throw be;
+			}
+			
+			warehouseBO.setActive(false);
+			et.commit();
+			res = warehouseBO.getId();
+			
+		} catch (Exception e) {
+			if (e instanceof BusinessException) {
+				et.rollback();
+			}
+			else {
+				res = Errors.UnespectedError;
+			}
+		} finally {
+			em.close();
+		}
+		return res;
+	}
+	
+	private boolean isValid(WarehouseTransfer warehouse) {
+		return warehouse != null &&
+				SintaxChecker.isCity(warehouse.getCity()) &&
+				SintaxChecker.isName(warehouse.getName());
+	}
+	
 }
