120609c79c49cb462eead37b3ce80a4cf94ee710
danielfdez17 <daniel17dev@gmail.com>
2023-12-24 11:47:55 +0100

    providerProductTransfer added

---
+++ src/business/providerProduct/ProviderProductTransfer.java
@@ -0,0 +1,48 @@
+package business.providerProduct;
+
+import utilities.Utils;
+
+public class ProviderProductTransfer {
+	
+	private int providerId;
+	private int productId;
+	private int amount;
+	
+	public ProviderProductTransfer(int providerId, int productId, int amount) {
+		super();
+		this.providerId = providerId;
+		this.productId = productId;
+		this.amount = amount;
+	}
+
+	public int getProviderId() {
+		return providerId;
+	}
+
+	public void setProviderId(int providerId) {
+		this.providerId = providerId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Provider ID: " + this.providerId + Utils.JUMP +
+				"Product ID: " + this.productId + Utils.JUMP;
+	}
+	
+}
