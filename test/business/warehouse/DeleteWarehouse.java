9edbe23f0179b8b1832cbf646bfbf2e704bd9cb5
danielfdez17 <daniel17dev@gmail.com>
2023-12-27 21:10:51 +0100

    WarehouseAS tests done and OK

--- test/business/warehouse/DeleteWarehouse.java
+++ test/business/warehouse/DeleteWarehouse.java
@@ -8,35 +8,45 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import business.businessFactory.BusinessFactory;
+import business.entityManagerFactory.EMFFactory;
 import business.product.ProductAS;
 import business.product.ProductTransfer;
+import business.worker.FullTimeWorkerTransfer;
+import business.worker.WorkerAS;
 import utilities.Errors;
 
 public class DeleteWarehouse {
 	
-	private static final String city = "city";
+	private static final String city = "city", nif = "12345678";
 	private static final int stock = 19;
-	private static final double price = 38;
+	private static final double price = 38, salary = 83;
 	
 	private static BusinessFactory bf;
 	private static WarehouseAS warehouseAS;
 	private static ProductAS productAS;
+	private static WorkerAS workerAS;
 	private WarehouseTransfer warehouse;
 	private ProductTransfer product;
-	private int warehouseId, productId;
+	private FullTimeWorkerTransfer fullTime;
+	private int warehouseId, productId, workerId;
 	
 	@BeforeClass public static void setUp() {
 		bf = BusinessFactory.getInstance();
 		warehouseAS = bf.createWarehouseAS();
 		productAS = bf.createProductAS();
+		workerAS = bf.createWorkerAS();
+		EMFFactory.getInstance();
 	}
 	
-	private void setASs(String name) {
+	private void setASs(String name, String nif) {
 		warehouse = new WarehouseTransfer(name, name);
 		warehouseId = warehouseAS.createWarehouse(warehouse);
 		
 		product = new ProductTransfer(name, stock, price, warehouseId);
 		productId = productAS.createProduct(product);
+		
+		fullTime = new FullTimeWorkerTransfer(nif, name, warehouseId, salary);
+		workerAS.createFullTimeWorker(fullTime);
 	}
 
 	@Test public void deleteOK() {
@@ -58,11 +68,14 @@ public class DeleteWarehouse {
 	
 	@Test public void deleteKOWithActiveProducts() {
 		String name = "deleteWarehouseKOWarehouseWithActiveProducts";
-		this.setASs(name);
+		this.setASs(name, nif + "A");
 		assertEquals(warehouseAS.deleteWarehouse(warehouseId), Errors.WarehouseWithActiveProducts);
 	}
 	
-	@Test public void deleteKOWithActiveWorkers() {
-		assertTrue(false);
+	@Test public void deleteWarehouseKOWithActiveWorkers() {
+		String name = "deleteWarehouseKOWithActiveWorkers";
+		this.setASs(name, nif + "B");
+		assertEquals(productAS.deleteProduct(productId), productId);
+		assertEquals(warehouseAS.deleteWarehouse(warehouseId), Errors.WarehouseWithActiveWorkers);
 	}
 }
