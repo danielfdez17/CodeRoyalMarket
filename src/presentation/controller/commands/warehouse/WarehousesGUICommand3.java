88d07ec79bc521a121bf50066cce8fc14ae6d84b
danielfdez17 <daniel17dev@gmail.com>
2024-01-03 19:53:19 +0100

    Workers & Warehouses Commands packages

---
+++ src/presentation/controller/commands/warehouse/WarehousesGUICommand3.java
@@ -0,0 +1,21 @@
+package presentation.controller.commands.warehouse;
+
+import presentation.controller.Events;
+import presentation.controller.commands.Command;
+import presentation.controller.view.Context;
+
+public class WarehousesGUICommand3 implements Command {
+
+	@Override
+	public Context execute(Object data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Events getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/presentation/controller/commands/worker/WorkersGUICommand3.java
@@ -0,0 +1,21 @@
+package presentation.controller.commands.worker;
+
+import presentation.controller.Events;
+import presentation.controller.commands.Command;
+import presentation.controller.view.Context;
+
+public class WorkersGUICommand3 implements Command {
+
+	@Override
+	public Context execute(Object data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Events getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
